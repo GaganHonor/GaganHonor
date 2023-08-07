80d77e59877cfa9ad93d89b3af1ab0775630d2d5
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-07 22:00:13 +0530

    Add files via upload

---
+++ lists/projects.js
@@ -0,0 +1,58 @@
+export const projects = {
+  "Discipulis": {
+    "Link": "https://discipulis.vercel.app/",
+    "Desc": "a websited aimed to help people learn Latin in a fun way",
+    "Tags": ["Next.js", "React.js", "MongoDB", "Education"]
+  },
+  "What If": {
+    "Link": "https://what-if-scenarios.vercel.app",
+    "Desc": "a website where you will face hypothetical scenarios",
+    "Tags": ["Next.js", "React.js", "MongoDB", "Forum", "NextAuth"]
+  },
+  "Melody Maker": {
+    "Link": "https://melody-maker-theta.vercel.app/",
+    "Desc": "instantly generates sheet music and audio for simple melodies in any key",
+    "Tags": ["Next.js", "React.js", "VexFlow", "Tone.js"]
+  },
+  "VulcanWM's GuestBook": {
+    "Link": "https://vulcanwm-guestbook.vercel.app/",
+    "Desc": "view and sign my guestbook",
+    "Tags": ['Next.js', 'React.js', 'MongoDB', "NextAuth"]
+  },
+  "Global CSS Art": {
+    "Link": "https://global-css-art.vercel.app/",
+    "Desc": "a community dedicated to bringing together CSS artists",
+    "Tags": ['Next.js', 'CSS', 'GitHub', 'Open-Source']
+  },
+  "Distribute Time": {
+    "Link": "https://distribute-time.onrender.com/",
+    "Desc": "automatically make your own timetable",
+    "Tags": ["Flask", "HTML,CSS,JS", "Tool"]
+  }, 
+  "OvalEyes": {
+    "Link": "https://ovaleyes.ovaleyes.repl.co",
+    "Desc": "a social media website",
+    "Tags": ["Flask", "HTML,CSS,JS", "MongoDB", "Forum"]
+  },
+  "Escape the Punch": {
+    "Link": "https://escape-the-punch.netlify.app/",
+    "Desc": "try to escape the ghost's punch",
+    "Tags": ["kaboom.js", 'Game']
+  },
+  "wizard vs. snake": {
+    "Link": "https://wizard-vs-snake.vulcanwm.repl.co/",
+    "Desc": "escape the snakes while exploring wizard world",
+    "Tags": ['kaboom.js', 'express.js', 'HTML,CSS,JS', 'ReplitDB', 'ReplAuth']
+  }, 
+  // "Munity": {
+  //   "Link": "https://munity.vulcanwm.repl.co",
+  //   "Desc": "test yourself on song lyrics",
+  //   "Status": "progress",
+  //   "Tags": ["Flask", "HTML,CSS,JS", "MongoDB", "Game"]
+  // }, 
+  "Jasonism": {
+    "Link": "https://jasonism.vulcanwm.repl.co",
+    "Desc": "make your block of wood mighty",
+    "Tags": ['Flask', 'HTML,CSS,JS', 'MongoDB', "Game"]
+  }
+}
---
+++ lists/skills.js
@@ -0,0 +1,15 @@
+// max 8 with this styling
+export const skills = {
+    "Languages": ['Python', 'JavaScript', 'JSX', 'TypeScript', 'Swift'], 
+    "Web Frameworks": ["Flask", 'React.js', 'AFrame', 'Kaboom.js', 'Express.js', "Svelte"], 
+    "Mobile Frameworks": ["React Native", "Expo"],
+    "Full Stack Frameworks": ["Next.js", "SvelteKit"],
+    "Databases": ["MongoDB", "CosmosDB", "Key Value Databases"],
+    "Types of APIs": ['REST', "GraphQL"], 
+    "APIs": ['Spotify API', 'Gmail API', 'LyricsGenius'], 
+    "Tooling": ["Git", "GitHub", "pip", "Vercel", "Heroku", "npm"], 
+    "Data Format": ['Form Data', 'JSON', "XML", "CSV"], 
+    "Markup": ["HTML", "Markdown"], 
+    "Styling": ['CSS', "Styled-JSX"], 
+    "Analytics": ['Google Analytics', "Vercel Audiences"]
+  }
